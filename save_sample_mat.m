82f529aeca6271d2ee35e14a9a181ee6cd2e13ce
Alex778 <142656024+boynextdoor778@users.noreply.github.com>
2026-05-05 16:23:00 +0800

    Delete save_sample_mat.m

--- save_sample_mat.m
+++
@@ -1,6 +0,0 @@
-function save_sample_mat(matFile, voxel, density, radius, bendAmp, nPtsPerArm, refAxis, nVoxel, topoFile)
-% Save one accepted sample from parfor safely.
-voxel = logical(voxel);
-save(matFile, 'voxel', 'density', 'radius', 'bendAmp', ...
-    'nPtsPerArm', 'refAxis', 'nVoxel', 'topoFile');
-end
